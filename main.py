34badc6decde0ac63737da4496a7f8b5eb233ce0
Paul <pocketry@github>
2025-04-30 15:52:53 -0700

    return the santa list for a given exchange

---
+++ main.py
@@ -0,0 +1,30 @@
+# from typing import Union
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+from santaRepository import santaRepository
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: bool | None = None
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/santalist/{exchange}")
+def get_santas(exchange: str):
+    return santaRepository(exchange).getSantas(exchange)
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, b: str | None = None):
+    return {"item_id": item_id, "q": b}
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_price": item.price, "item_id": item_id}
--- readme.md
+++ readme.md
@@ -1,1 +1,6 @@
 A python scirpt that uses classes and csv files to create a secret santa exchange and email participants their assignments
+
+### Development
+
+To start the server
+`fastapi dev main.py`
